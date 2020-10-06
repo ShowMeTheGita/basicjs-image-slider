f76cf55e53f02f4a8d9c67010d0481999e6b2040
Guilherme Silva <guilherme.m.m.f.silva@gmail.com>
2020-10-06 22:03:16 +0100

    added image adding functionality to button

---
+++ script.js
@@ -0,0 +1,9 @@
+const prevBtn = document.querySelector('.prevBtn');
+const container = document.querySelector('.images')
+
+prevBtn.addEventListener('click', function() {
+    container.style.backgroundImage = "url(https://i.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA)";
+})
+
+
+
